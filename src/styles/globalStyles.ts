a6cf42422aecf9982b42062027c873f2d8ba6689
Lucas Silva <lucassilva.uern@gmail.com>
2022-07-28 14:15:12 -0300

    reset css

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,9 +2,9 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    background-color: wheat;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    outline: none;
   }
 `;
